6f67a300d0f7e2b4241eebaff98078f7f94072c4
Ouroboros <ouroboros@users.noreply.github.com>
2026-03-05 03:47:17 +0000

    fix: properly export ToolResult from tools/__init__.py

--- ouroboros/tools/__init__.py
+++ ouroboros/tools/__init__.py
@@ -1,4 +1,3 @@
-from .registry import ToolRegistry, ToolContext, ToolEntry, ToolCall
+from .registry import ToolRegistry, ToolEntry, ToolResult
 
-def get_tools():
-    return ToolRegistry.get_instance().get_all_tools()
+__all__ = ['ToolRegistry', 'ToolEntry', 'ToolResult']
